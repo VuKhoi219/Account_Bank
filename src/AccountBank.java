87867e16e46b0e6c00bcdd80a33448d44d84dceb
vukhoi2k5 <vukhoi353@gmail.com>
2024-05-21 23:46:10 +0700

    em nộp lại bài ạ

--- src/AccountBank.java
+++ src/AccountBank.java
@@ -1,12 +1,88 @@
 public class AccountBank {
-    public static void main(String[] args) {
-        ControlAccountInfo accountInfo = new ControlAccountInfo();
-        accountInfo.insertAccount();
-        accountInfo.displayAccountInfo();
-        accountInfo.accountDeposit ();
-        accountInfo.displayAccountInfo();
-        accountInfo.accountWithdraw();
-        accountInfo.displayAccountInfo();
-        accountInfo.Transfer();
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public void displayAccountInfo(String accountNumber, String accountHolderName, double balance) {
+        System.out.println("Account Number: " + this.accountNumber);
+        System.out.println("Account Holder Name: " + this.accountHolderName);
+        System.out.println("Balance: " + this.balance);
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.err.println("Amount can't be negative");
+            return;
+        } else {
+            this.balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= 0) {
+            System.err.println("Amount can't be negative");
+            return;
+        }
+        if (amount > balance) {
+            System.err.println("Insufficient balance");
+            return;
+        } else {
+            this.balance -= amount;
+        }
+    }
+
+    public void transfer(String destinationAccountNumber, double amount) {
+
+        if (amount <= 0) {
+            System.err.println("Amount can't be negative");
+            return;
+        }
+        if (amount > balance) {
+            System.err.println("Insufficient balance");
+            return;
+        }
+        int destinationAccountIdex = -1;
+        for (int i = 0; i < Main.accountBanks.size(); i++) {
+            AccountBank destinationAccountBank = Main.accountBanks.get(i);
+            if (destinationAccountBank.getAccountNumber().equals(destinationAccountNumber)) {
+                destinationAccountIdex = i;
+            }
+        }
+        if (destinationAccountIdex == -1) {
+            System.err.println("No such account number");
+        }
+        balance -= amount;
+        AccountBank destinationAccount = Main.accountBanks.get(destinationAccountIdex);
+        destinationAccount.setBalance(destinationAccount.balance + amount);
+    }
+
+    public AccountBank() {
+        this.accountNumber = "";
+        this.accountHolderName = "";
+        this.balance = 0;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,59 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+    public static ArrayList<AccountBank> accountBanks = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Account Bank");
+        System.out.println("Please enter your account number:");
+        String accountNumber = scanner.nextLine();
+        System.out.println("Please enter your account holder name:");
+        String accountHolderName = scanner.nextLine();
+        System.out.println("Please enter your balance:");
+        double balance = scanner.nextDouble();
+        scanner.nextLine();
+        AccountBank accountBank = new AccountBank();
+        accountBanks.add(accountBank);
+        accountBank.setAccountHolderName(accountHolderName);
+        accountBank.setAccountNumber(accountNumber);
+        accountBank.setBalance(balance);
+        // Test display
+        System.out.println("--------------------");
+        System.out.println("Test Display Account Info ");
+        accountBank.displayAccountInfo(accountNumber, accountHolderName, balance);
+        // Test deposit
+        System.out.println("--------------------");
+        System.out.println("Test deposit method");
+        double deposit = scanner.nextDouble();
+        System.out.println("Deposit amount: " + deposit);
+        accountBank.deposit(deposit);
+        System.out.println("Your account balance is: " + accountBank.getBalance());
+        // Test withdraw
+        System.out.println("--------------------");
+        System.out.println("Test withdrawal method");
+        double withdrawal = scanner.nextDouble();
+        System.out.println("Withdrawal amount: " + withdrawal);
+        accountBank.withdraw(withdrawal);
+        System.out.println("Your account balance is: " + accountBank.getBalance());
+        //Test transfer
+        System.out.println("--------------------");
+        System.out.println("Test transfer method");
+        AccountBank destinationAccountBank = new AccountBank();
+        destinationAccountBank.setAccountHolderName("Vũ");
+        destinationAccountBank.setAccountNumber("2345");
+        destinationAccountBank.setBalance(20000);
+        accountBanks.add(destinationAccountBank);
+        accountBanks.add(accountBank);
+        destinationAccountBank.displayAccountInfo(accountNumber, accountHolderName, balance);
+        scanner.nextLine();
+        System.out.println("Enter the transfer account number");
+        String transferAccountNumber = scanner.nextLine();
+        System.out.println("Enter transfer amount");
+        double transfer = scanner.nextDouble();
+        accountBank.transfer(transferAccountNumber, transfer);
+        System.out.println("My account: " + accountBank.getBalance());
+        System.out.println("Destination account :" + destinationAccountBank.getBalance());
     }
 }
